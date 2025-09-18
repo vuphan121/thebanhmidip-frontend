466871411d7468cdfb2423969a63bfd21fb58b70
Vu Phan <157292772+vuphan121@users.noreply.github.com>
2025-09-18 23:59:33 +0700

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../view/Home.vue'
-import ArticlePage from "../view/ArticlePage.vue";
+import Home from '@/view/Home.vue'
+//import RegisterPage from '@/view/RegisterPage.txt'
+import ArticlePage from '@/view/ArticlePage.vue'
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Home
-    },
-    {
-        path: '/article/:id',
-        name: 'ArticlePage',
-        component: ArticlePage,
-        props: true
-    }
+    { path: '/', name: 'Home', component: Home },
+    // { path: '/register', name: 'Register', component: RegisterPage },
+    { path: '/article/issue/:id', name: 'Article', component: ArticlePage }
 ]
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
-
-export default router
+export default createRouter({ history: createWebHistory(), routes })
