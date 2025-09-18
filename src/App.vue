7687a2facc3e3423ee69801f8a5824786d2f11ae
vuphan121 <157292772+vuphan121@users.noreply.github.com>
2025-09-18 23:59:22 +0700

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts">
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  height: 100%;
+  font-family: 'Inter', sans-serif;
+  background-color: #F7C3A1;
+}
+
+*, *::before, *::after {
+  box-sizing: inherit;
+}
+</style>
